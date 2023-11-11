16a19b3556cf225751108ae3276a3e1c8e4fcf23
Julien <julien.bouisset@shine.fr>
2023-11-11 06:14:18 +0100

    added save panels

--- lib/html.py
+++ lib/html.py
@@ -1,11 +1,9 @@
-
-
 import json
 
 
 class HTML:
-	def header(title='',reldir=''):
-		return """<!DOCTYPE html>
+    def header(title="", reldir=""):
+        return """<!DOCTYPE html>
 <html>
 
 <head>
@@ -28,16 +26,17 @@ class HTML:
 <body>
 <h1>{title}</h1>
 
-""".format(title=title,reldir=reldir)
-	
-	
-	def nbdiffs(files_diff):
-		return "<p>{0} differences found in files</p>".format(len(files_diff))
-	
-	
-	pageId = 0
-	def side_by_side_panels(title,step_info,jsons,v1,v2,images_dir,known_panels):
-		html = """
+""".format(
+            title=title, reldir=reldir
+        )
+
+    def nbdiffs(files_diff):
+        return "<p>{0} differences found in files</p>".format(len(files_diff))
+
+    pageId = 0
+
+    def side_by_side_panels(title, step_info, jsons, v1, v2, images_dir, known_panels):
+        html = """
 			<h2>{0}</h2>
 			<p class="step-info">{1}</p>
 			<div class="sidebyside">
@@ -45,9 +44,11 @@ class HTML:
 				<div class="version">{3}</div>
 			</div>
 			<div class="sidebyside">
-		""".format(title,step_info,v1,v2)
-		
-		oneside = """
+		""".format(
+            title, step_info, v1, v2
+        )
+
+        oneside = """
 			<div id="page{id}" class="kumiko-reader halfwidth debug"></div>
 			<script type="text/javascript">
 				var reader = new Reader({{
@@ -59,26 +60,31 @@ class HTML:
 				reader.start();
 			</script>
 			"""
-		i = -1
-		for js in jsons:
-			i += 1
-			html += oneside.format(id=HTML.pageId,json=json.dumps(js),images_dir=images_dir,known_panels=known_panels[i])
-			HTML.pageId += 1
-		
-		html += '</div>'
-		return html
-	
-	
-	def imgbox(images):
-		html = "<h3>Debugging images</h3>\n<div class='imgbox'>\n";
-		for img in images:
-			html += "\t<div><p>{}</p><img src='{}' /></div>\n".format(img['label'],img['filename'])
-		
-		return html + "</div>\n\n"
-	
-	
-	def reader(js,images_dir):
-		return """
+        i = -1
+        for js in jsons:
+            i += 1
+            html += oneside.format(
+                id=HTML.pageId,
+                json=json.dumps(js),
+                images_dir=images_dir,
+                known_panels=known_panels[i],
+            )
+            HTML.pageId += 1
+
+        html += "</div>"
+        return html
+
+    def imgbox(images):
+        html = "<h3>Debugging images</h3>\n<div class='imgbox'>\n"
+        for img in images:
+            html += "\t<div><p>{}</p><img src='{}' /></div>\n".format(
+                img["label"], img["filename"]
+            )
+
+        return html + "</div>\n\n"
+
+    def reader(js, images_dir):
+        return """
 			<div id="reader" class="kumiko-reader fullpage"></div>
 			<script type="text/javascript">
 				var reader = new Reader({{
@@ -89,11 +95,12 @@ class HTML:
 				}});
 				reader.start();
 			</script>
-			""".format(json=js,images_dir=images_dir)
-	
-	
-	footer = """
+			""".format(
+            json=js, images_dir=images_dir
+        )
+
+    footer = """
 
 </body>
 </html>
-""" 
+"""
